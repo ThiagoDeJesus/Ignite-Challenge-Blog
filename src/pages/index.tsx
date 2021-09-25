52c798ba107a65f532f5797253331e61c6905abd
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-09-24 21:39:18 -0300

    feat: add pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +25,61 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <header className={styles.header}>
+        <img src="/logo.svg" alt="logo" />
+      </header>
+      <main className={styles.main}>
+        <ul className={styles.listaDePosts}>
+          <li className={styles.post}>
+            <Link href="/post/slugDoPost">
+              <a>
+                <h2>Como utilizar Hooks</h2>
+                <p>Pensando em sincronização em vez de ciclos de vida</p>
+                <div>
+                  <time>15 Mar 2021</time>
+                  <span className={commonStyles.author}>Joseph Oliveira</span>
+                </div>
+              </a>
+            </Link>
+          </li>
+          <li className={styles.post}>
+            <Link href="/post/slugDoPost">
+              <a>
+                <h2>Como utilizar Hooks</h2>
+                <p>Pensando em sincronização em vez de ciclos de vida</p>
+                <div>
+                  <time>15 Mar 2021</time>
+                  <span>Joseph Oliveira</span>
+                </div>
+              </a>
+            </Link>
+          </li>
+          <li className={styles.post}>
+            <Link href="/post/slugDoPost">
+              <a>
+                <h2>Como utilizar Hooks</h2>
+                <p>Pensando em sincronização em vez de ciclos de vida</p>
+                <div>
+                  <time>15 Mar 2021</time>
+                  <span>Joseph Oliveira</span>
+                </div>
+              </a>
+            </Link>
+          </li>
+        </ul>
+        <button
+          type="button"
+          className={`${commonStyles.highlight} ${styles.carregarMais}`}
+        >
+          Carregar mais posts
+        </button>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
